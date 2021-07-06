7b4d17750402526ea361919d635a83825bd63e8a
Andrew Stepanets <andrii.stepanets@vertexinc.com>
2021-07-06 14:30:50 +0300

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+
+const  App:React.FC = () => {
+  const todos = [
+    {
+        id: 't1',
+        text: 'Finish the course'
+    }
+]
+  return (
+    <div className="App">
+      <NewTodo />
+      <TodoList items={todos}/>
+    </div>
+  );
+}
+
+export default App;
